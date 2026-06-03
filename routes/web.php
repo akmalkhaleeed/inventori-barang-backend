f5c2d858f4fbd9dca8fe6f0533000c8d6c4edca2
akmalkhaleeed <2303010112@unper.ac.id>
2026-06-03 23:36:06 +0700

    feat: pembuatan endpoint api login dan seeder data user

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,11 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
+// Jalur tes biasa
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->post('/api/login', 'AuthController@login');
+
+// Tulis manual dan polos tanpa group prefix
+$router->post('api/login', 'AuthController@login');
+$router->post('/api/login', 'AuthController@login'); // Kita pasang dua-duanya biar kalau salah satu gak kebaca, yang satunya lolos!
